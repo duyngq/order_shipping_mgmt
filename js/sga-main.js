0cb48389356ea79bfa74d5502dc3c4a6861dfb97
duyngq <lonelyemerald@gmail.com>
2017-10-18 00:33:03 +0700

    handle index page

--- js/sga-main.js
+++ js/sga-main.js
@@ -1,8 +1,13 @@
 // The root URL for the RESTful services
-var HOST = "localhost";
-var PORT = "8808";
-var rootURL = "http://" + HOST + ":" + PORT + "/api";
+// This is standard host on linux
+//var HOST = "localhost";
 
+//For wamp, there is an issue, so the host should be as:
+// refer this link: https://stackoverflow.com/questions/29945153/php-slim-api-404-not-found
+var HOST="localhost/order"
+var PORT = "";
+// var rootURL = "http://" + HOST + ":" + PORT + "/api";
+var rootURL = "http://localhost/order/api"
 var currentOrder;
 
 // Retrieve wine list when application starts
@@ -23,43 +28,41 @@ $('#btnDelete').hide();
 /**
  * Define action to connect/get data from server
  */
-
-/*function findAll() {
-    console.log('find all orders');
-    $.ajax({
-        type: 'GET',
-        url: rootURL + "/orders/all",
-        dataType: "json", // data type of response
-        success: renderTableData,
-        // error: alert("failed")
-    });
-}*/
-
 function findAll() {
     console.log('find all orders');
     $.ajax({
         type: 'GET',
         url: rootURL + "/orders/all",
         dataType: "json", // data type of response
-        success: renderTableData
-        // error: alert("failed")
+        success: renderTableData,
+        error: function(data) {
+            console.log("failed", data);
+        }
     });
 }
 
 /**
  * Render JSON data getting from server and render to html table
+ * Will render for:
+ * 1. ordered data
+ * 2. shipping data
+ * 3. delivered data
  * @param data
  */
 function renderTableData(data) {
     // JAX-RS serializes an empty list as null, and a 'collection of one' as an object (not an 'array of one')
     var list = data == null ? [] : (data.orders instanceof Array ? data.orders : [data.orders]);
     var table = $('#example').DataTable();
+    // var shippingTable = $('#shippingTbl').DataTable();
+    var deliveredTable = $('#deliveredTbl').DataTable();
 
     table.clear().draw();
-    var totalWeight = 0, totalAmount = 0;
+    deliveredTable.clear().draw();
+    // var totalWeight = 0, totalAmount = 0;
+    // 1. ordered data
     $.each(list, function (index, order) {
-        var weight = order.weight;
-        var total = order.total;
+        // var weight = order.weight;
+        // var total = order.total;
         /* this will validate if value is not:
             null
             undefined
@@ -68,56 +71,16 @@ function renderTableData(data) {
             false
             0
         */
-        if (weight)
-            totalWeight += parseFloat(weight);
-        if (total)
-            totalAmount += parseFloat(total);
-        table.row.add([order.id, order.date, order.s_name, order.s_phone, order.s_address, order.r_name, order.r_phone, order.r_address, order.weight, order.total]).draw();
+        // if (weight)
+        //     totalWeight += parseFloat(weight);
+        // if (total)
+        //     totalAmount += parseFloat(total);
+        if (order.status == 1)
+            table.row.add([order.id, order.date, order.s_name, order.s_phone, order.s_address, order.r_name, order.r_phone, order.r_address, order.weight, order.total]).draw();
+        else if (order.status == 0)
+            deliveredTable.row.add([order.id, order.date, order.s_name, order.s_phone, order.s_address, order.r_name, order.r_phone, order.r_address, order.weight, order.total]).draw();
     });
-    $("#example tfoot th#orderedWeight").html(totalWeight);
+    // $("#example tfoot th#orderedWeight").html(totalWeight);
     // $("#weight").html(totalWeight); same result
-    $("#example tfoot th#orderedAmount").html(totalAmount);
-}
-
-/**
- * Render JSON data getting from server and render to html
- * @param data
- */
-function renderList(data) {
-    // JAX-RS serializes an empty list as null, and a 'collection of one' as an object (not an 'array of one')
-    var list = data == null ? [] : (data.orders instanceof Array ? data.orders : [data.orders]);
-
-    $('#data-primary div').remove();
-    $.each(list, function (index, order) {
-        var eachData = '<div class="row data-row-border" data-identity="' + order.id + '">' +
-            '        <div class="col-xs-1">' +
-            '        <div class="row">' +
-            '        <div class="col-xs-4 nopadding">' + order.id + '</div>' +
-            '        <div class="col-xs-4 nopadding">' + order.date + '</div>' +
-            '        </div></div>' +
-            '        <div class="col-xs-1 word-wrap">' + order.cust_name + '</div>' +
-            '        <div class="col-xs-1 word-wrap">' + order.phone + '</div>' +
-            '        <div class="col-xs-2 word-wrap">' + order.address + '</div>' +
-            '        <div class="col-xs-1 word-wrap">' + order.recv_name + '</div>' +
-            '        <div class="col-xs-1 word-wrap">' + order.phone + '</div>' +
-            '        <div class="col-xs-2 word-wrap">' + order.address + '</div>' +
-            '        <div class="col-xs-1">' + order.weight + '</div>' +
-            '        <div class="col-xs-1">' + order.id + '</div>' +
-            '        </div>';
-        $('#data-primary ').append(eachData);
-        // $('#data-primary ').append('<div class="row data-row-border" data-identity="' + order.id + '">');
-        // $('#data-primary ').append('<div class="col-xs-1">');
-        // $('#data-primary ').append('<div class="col-xs-1">'+order.id+'</div>');
-        // $('#data-primary ').append('<div class="col-xs-1">'+order.date+'</div>');
-        // $('#data-primary ').append('</div>');
-        // $('#data-primary ').append('<div class="col-xs-1 word-wrap">'+order.cust_name+'</div>');
-        // $('#data-primary ').append('<div class="col-xs-1 word-wrap">'+order.phone+'</div>');
-        // $('#data-primary ').append('<div class="col-xs-2 word-wrap">'+order.address+'</div>');
-        // $('#data-primary ').append('<div class="col-xs-1 word-wrap">'+order.recv_name+'</div>');
-        // $('#data-primary ').append('<div class="col-xs-1 word-wrap">'+order.phone+'</div>');
-        // $('#data-primary ').append('<div class="col-xs-2 word-wrap">'+order.address+'</div>');
-        // $('#data-primary ').append('<div class="col-xs-1">'+order.weight+'</div>');
-        // $('#data-primary ').append('<div class="col-xs-1">'+order.id+'</div>');
-        // $('#data-primary ').append('</div>');
-    });
+    // $("#example tfoot th#orderedAmount").html(totalAmount);
 }
